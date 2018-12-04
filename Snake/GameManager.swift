47ee9f58ff05fc6de79799a7d34286a63f336d26
Luciana Lordelo <lulordelo@gmail.com>
2018-12-04 21:09:40 +0100

    Initial commit

---
+++ Snake/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Luciana Lordelo Nascimento on 04/12/2018.
+//  Copyright © 2018 Luciana Lordelo Nascimento. All rights reserved.
+//
+
+import Foundation
